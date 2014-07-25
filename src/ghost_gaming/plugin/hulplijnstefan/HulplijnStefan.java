8c7449a2c6e9955ec234ba22505f885316908321
Matthijs <matthijs.booman@hotmail.com>
2014-07-25 17:31:20 +0200

    /stefan works \o/

--- src/ghost_gaming/plugin/hulplijnstefan/HulplijnStefan.java
+++ src/ghost_gaming/plugin/hulplijnstefan/HulplijnStefan.java
@@ -6,6 +6,8 @@ import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class HulplijnStefan extends JavaPlugin {
+	
+	public static final String pluginMSG = "[HulplijnStefan] ";
 
 	@Override
 	public void onEnable() {
@@ -19,13 +21,18 @@ public class HulplijnStefan extends JavaPlugin {
 	
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 	
-		if (cmd.getName().equalsIgnoreCase("stefan") && sender instanceof Player) {
+		if (cmd.getName().equalsIgnoreCase("stefan")) {
+			if(sender instanceof Player){
+				Player player = (Player)sender;
+				
+				player.sendMessage(pluginMSG + "Your question wil be awnserd as soon as possible!");
+				
+				return true;
+			}else{
+				sender.sendMessage(pluginMSG + "Player Command Only!");
+				return true;
+			}
 			
-			Player player = (Player)sender;
-			
-			player.sendMessage("Your question wil be awnserd as soon as possible");
-			
-			return true;
 		}
 		
 		return false;

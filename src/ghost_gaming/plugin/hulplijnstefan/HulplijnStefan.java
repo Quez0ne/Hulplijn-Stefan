f7bda2a2615cb602153ee6ad87c26c110cc3a297
quez0ne <quinten.tieman@hotmail.com>
2014-07-25 16:26:18 +0200

    Updated some code

--- src/ghost_gaming/plugin/hulplijnstefan/HulplijnStefan.java
+++ src/ghost_gaming/plugin/hulplijnstefan/HulplijnStefan.java
@@ -2,6 +2,7 @@ package ghost_gaming.plugin.hulplijnstefan;
 
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class HulplijnStefan extends JavaPlugin {
@@ -18,6 +19,14 @@ public class HulplijnStefan extends JavaPlugin {
 	
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 	
+		if (cmd.getName().equalsIgnoreCase("stefan") && sender instanceof Player) {
+			
+			Player player = (Player)sender;
+			
+			player.sendMessage("Your question wil be awnserd as soon as possible");
+			
+			return true;
+		}
 		
 		return false;
 	}
